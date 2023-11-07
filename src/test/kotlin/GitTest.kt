ac90049fb270daca497c312a389cf50fe71cfbff
LilyStilson <lunamusicoff@gmail.com>
2023-11-07 23:38:19 +0100

    Added documentation; Improved interactions without creating of commit tree; Added additional tests.

--- src/test/kotlin/GitTest.kt
+++ src/test/kotlin/GitTest.kt
@@ -1,26 +1,17 @@
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
 class GitTest {
-    private var git: Git? = null
-    private var tree: Tree? = null
-
-    @Test
-    fun testInit() {
-        git = Git()
-        assert(git != null)
-
-        tree = Tree()
-        assert(tree != null)
-    }
     @Test
     fun testCommit() {
-        git = git ?: Git()
-        tree = tree ?: Tree()
+        val git = Git()
+        val tree = Tree()
 
-        tree!!.entries["file.txt"] = Either.Left(Blob("Hello, world!"))
-        git!!.commit(tree!!, "John Doe", "Initial commit")
+        git.addFiles(tree, mapOf("file.txt" to Either.Left(Blob("Hello, world!"))))
+        git.commit(tree, "John Doe", "Initial commit")
 
-        val commits = git!!.listCommits()
+
+        val commits = git.listCommits()
         assert(commits.size == 1)
         assert(commits[0].author == "John Doe")
         assert(commits[0].message == "Initial commit")
@@ -29,34 +20,64 @@ class GitTest {
 
     @Test
     fun testFindCommitByHash() {
-        git = git ?: Git()
-        tree = tree ?: Tree()
+        val git = Git()
+        val tree = Tree()
 
-        tree!!.entries["file.txt"] = Either.Left(Blob("Hello, world!"))
-        git!!.commit(tree!!, "John Doe", "Initial commit")
+        git.addFiles(tree, mapOf("file.txt" to Either.Left(Blob("Hello, world!"))))
+        git.commit(tree, "John Doe", "Initial commit")
 
-        val commits = git!!.listCommits()
-        val commit = git!!.findCommitByHash(commits[0].hash)
+        val commits = git.listCommits()
+        val commit = git.findCommitByHash(commits[0].hash)
         assert(commit != null)
         assert(commit == commits[0])
     }
 
     @Test
     fun testTreeCommit() {
-        git = git ?: Git()
-        tree = tree ?: Tree()
+        val git = Git()
+        val tree = Tree()
 
         val commitTree = Tree()
-        commitTree.entries["file.txt"] = Either.Left(Blob("Hello, world!"))
 
-        tree!!.entries["file.txt"] = Either.Right(commitTree)
-        git!!.commit(tree!!, "John Doe", "Initial commit")
+        git.addFiles(tree, mapOf("file.txt" to Either.Right(commitTree)))
+        git.commit(tree, "John Doe", "Initial commit")
 
-        val commits = git!!.listCommits()
+        val commits = git.listCommits()
         assert(commits.size == 1)
         assert(commits[0].author == "John Doe")
         assert(commits[0].message == "Initial commit")
         assert(tree == commits[0].tree)
+    }
+
+    @Test
+    fun testCommitWithPrivateTree() {
+        val git = Git()
+
+        git.addFiles(files = mapOf("file.txt" to Either.Left(Blob("Hello, world!"))))
+        git.commit(author = "John Doe", message = "Initial commit")
+
+        val commits = git.listCommits()
+        assert(commits.size == 1)
+        assert(commits[0].author == "John Doe")
+        assert(commits[0].message == "Initial commit")
+    }
+
+    @Test
+    fun testTwoCommitsWithPrivateTree() {
+        val git = Git()
+
+        git.addFiles(files = mapOf("file.txt" to Either.Left(Blob("Hello, world!"))))
+        git.commit(author = "John Doe", message = "Initial commit")
+
+        git.addFiles(files = mapOf("file.txt" to Either.Left(Blob("Hello, world!"))))
+        git.commit(author = "John Doe", message = "2nd commit")
+
+        val commits = git.listCommits()
+        assert(commits.size == 2)
+        assert(commits[0].author == "John Doe")
+        assert(commits[0].message == "Initial commit")
+        assert(commits[1].author == "John Doe")
+        assert(commits[1].message == "2nd commit")
     }
 
     @Test
@@ -70,5 +91,18 @@ class GitTest {
         val tree2 = Tree()
         tree2.entries["file.txt"] = Either.Left(blob2)
         assert(tree1.hash == tree2.hash)
+    }
+
+    @Test
+    fun testCatchEmptyTree() {
+        val git = Git()
+
+        assertThrows<IllegalStateException> {
+            git.commit(Tree(), "John Doe", "Initial commit")
+        }
+
+        assertThrows<IllegalStateException> {
+            git.commit(author = "John Doe", message = "Initial commit")
+        }
     }
 }
